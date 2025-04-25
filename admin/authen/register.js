7e0eb40a47d572923183abd9fc4d78b9a3b22ba5
trucalice <nguyenthithanhhuong99992006@gmail.com>
2025-04-25 11:41:40 +0700

    buoi_2

---
+++ admin/authen/register.js
@@ -0,0 +1,22 @@
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match!');
+        return;
+    }
+
+    // TODO: Add API call to register user
+    console.log('Registration form submitted', {
+        firstName,
+        lastName,
+        email,
+        password
+    });
+}); 
